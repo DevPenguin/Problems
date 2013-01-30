d25bb0b1d704e75e5a8284da9f4098938ceebd07
Justin <jyang772@gmail.com>
2013-01-29 18:43:53 -0800

    Update Prob1.cpp
    Added a if statement, so that if the initial array size is passed then it will increase array size. 

--- Prob1.cpp
+++ Prob1.cpp
@@ -1,3 +1,5 @@
+/** Justin Yang was here. **/
+
 #include <iostream>
 
 using namespace std;
@@ -8,6 +10,30 @@ int main()
   int initialarray(5);
   double* values = new double[initialarray];
   double* temp(0);
+  double inputvalue(0.0);   // Current input value
   int index(0);
-  for(;;)
+  for(;;)                        //indefinite loop
+{
+
+cout << "Please enter a value and/or 0 to end: ";
+cin >> inputvalue;
+
+if(inputvalue == 0.0)
+break;
+
+values[index++] = inputvalue;                    // index is incremented and inputvalue is stored in values
+
+if(index == intialarray);
+{
+  initialarray += 5;
+  temp = new double[initialarray];
+  
+    for(int i = 0 ; i<index ; i++)         // Copy old array elements to new
+   {     temp[i] = values[i];
 }
+      delete[] values;                       // Delete the old array
+      values = temp;                         // Store address of new array
+      temp = 0;                        // Reset temp to null
+    }
+  }
+
