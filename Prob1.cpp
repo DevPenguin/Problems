82ee71d0cf570f099565badf77b252c224c553de
Justin <jyang772@gmail.com>
2013-01-29 18:12:49 -0800

    Update Prob1.cpp
    Added temporary array storage. 

--- Prob1.cpp
+++ Prob1.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 
 using namespace std;
+
+
+int main()
+{
+  int initialarray(5);
+  double* values = new double[initialarray];
+  double* temp(0);
+  int index(0);
+  for(;;)
+}
