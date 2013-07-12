20f14605dea5020dcc644db53ce9b8b3f1fc09d6
Justin <jyang772@gmail.com>
2013-07-11 18:07:28 -0700

    Update Reverse Int Array.cpp

--- Reverse Int Array.cpp
+++ Reverse Int Array.cpp
@@ -2,16 +2,7 @@
 using namespace std;
 
 
-void Reverse(int numbers[], int count)
-{
-  for(int i=0; i < count/2 ; i++)
-  {
-    int j = count - i - 1;
-    int temp = numbers[i];
-    numbers[i] = numbers[j];
-    numbers[j] = temp;
-    }
-    }
+void Reverse(int numbers[], int count);
     
     int main()
     {
@@ -19,6 +10,29 @@ void Reverse(int numbers[], int count)
     int numbers[] = {1,2,3,4,5};
     int count = sizeof numbers / sizeof numbers[0];
     
+  	
+	Reverse(numbers, count);
+		
     return 0;
+    }
+
+
+
+void Reverse(int numbers[], int count)
+{
+	for(int i=0; i<count;i++)
+	cout << numbers[i];
+	
+	cout << endl;
+  for(int i=0; i < count/2 ; i++)
+  {
+    int j = count - i - 1;
+	int temp = numbers[i];
+    numbers[i] = numbers[j];
+    numbers[j] = temp;
+    }
+	
+	for(int i =0; i <count; i++)
+		cout << numbers[i];
     }
     
